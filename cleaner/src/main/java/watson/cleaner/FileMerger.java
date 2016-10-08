c2f4389b0f9075d4e3bb9f7c4634044e25a59233
Phillip Lopez <pgl5711@rit.edu>
2016-10-08 18:48:06 -0400

    Some minor updates to the Jsoup cleaner settings to retain more HTML tags. This approach seems to work better with Watson.

---
+++ cleaner/src/main/java/watson/cleaner/FileMerger.java
@@ -0,0 +1,36 @@
+package watson.cleaner;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Combines all of the files in the given directory
+ * into one single output file
+ *
+ */
+public class FileMerger {
+
+	public static void main(String args[]) throws IOException{
+		
+		String dirFrom = args[0];
+		String outFileName = args[1];
+		
+		File dir = new File(dirFrom);
+		PrintWriter out = new PrintWriter(new File(outFileName));
+		
+		for(File f : dir.listFiles()){
+			BufferedReader reader = new BufferedReader(new FileReader(f));
+			String line;
+			while((line = reader.readLine()) != null){
+				out.println(line);
+			}
+			reader.close();
+		}
+		out.flush();
+		out.close();
+	}
+	
+}
